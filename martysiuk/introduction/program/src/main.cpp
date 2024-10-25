f54bf52e16546146183e8a6a2aaf2d85b79e11e6
Kuzma Martysiuk <253854@ude.p.lodz.pl>
2024-10-25 11:03:54 +0200

    Made another modification

--- martysiuk/introduction/program/src/main.cpp
+++ martysiuk/introduction/program/src/main.cpp
@@ -8,11 +8,8 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
-	cout << "factorial(5) = " << factorial(1) << endl;
-	cout << "factorial(5) = " << factorial(2) << endl;
-	cout << "factorial(5) = " << factorial(3) << endl;
-	cout << "factorial(5) = " << factorial(4) << endl;
+	cout << "Introduction Project." << endl << endl;
+	cout << "Hello World!" << endl << endl;
 	cout << "factorial(5) = " << factorial(5) << endl;
 	return 0;
 }
